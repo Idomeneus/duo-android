ffbd086922cc06fbe8981fc1a22af86fe277df0e
ahabbaba9 <ahabbaba9@gmail.com>
2015-10-19 14:17:17 -0700

    Base test file for activity

---
+++ Duo/app/src/androidTest/kotlin/idomeneus/duo/MainActivityTest.kt
@@ -0,0 +1,27 @@
+package idomeneus.duo
+
+import android.app.Application
+import android.test.ApplicationTestCase
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+
+public class MainActivityTest {
+    Before
+    throws(Exception::class)
+    public fun setUp() {
+    }
+
+    Test
+    throws(Exception::class)
+    public fun testOnCreate() {
+    }
+
+    Test
+    throws(Exception::class)
+    public fun testOnOptionsItemSelected() {
+    }
+}
